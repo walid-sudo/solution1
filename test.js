90db710c671afeaac0f99e94f2f9d38f3221b439
walid-sudo <walidmohamed1704@gamil.com>
2023-10-09 08:25:24 +0300

    grade generater done

---
+++ test.js
@@ -0,0 +1,23 @@
+function calculateDemeritPoints(speed) {
+  const speedLimit = 70;
+  const kmPerDemeritPoint = 5;
+
+  if (speed < speedLimit) {
+      console.log("Ok");
+  } else {
+      const excessSpeed = speed - speedLimit;
+      const demeritPoints = Math.floor(excessSpeed / kmPerDemeritPoint);
+
+      if (demeritPoints > 12) {
+          console.log("License suspended");
+      } else {
+          console.log("Points:", demeritPoints);
+      }
+  }
+}
+
+// Get the speed as input (e.g., 80)
+const speed = 80;
+
+// Calculate and display demerit points
+calculateDemeritPoints(speed);
