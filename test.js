a28b256f50b222811a5172a22c34a246d2587d33
walid-sudo <walidmohamed1704@gamil.com>
2023-10-25 17:47:09 +0300

    Updated speed-test by using sync-prompt

--- test.js
+++ test.js
@@ -1,23 +1,28 @@
+const prompt = require('prompt-sync')();
+
 function calculateDemeritPoints(speed) {
   const speedLimit = 70;
   const kmPerDemeritPoint = 5;
 
-  if (speed < speedLimit) {
-      console.log("Ok");
+  if (speed <= speedLimit) {
+    console.log("Ok");
   } else {
-      const excessSpeed = speed - speedLimit;
-      const demeritPoints = Math.floor(excessSpeed / kmPerDemeritPoint);
+    const excessSpeed = speed - speedLimit;
+    const demeritPoints = Math.floor(excessSpeed / kmPerDemeritPoint);
 
-      if (demeritPoints > 12) {
-          console.log("License suspended");
-      } else {
-          console.log("Points:", demeritPoints);
-      }
+    if (demeritPoints > 12) {
+      console.log("License suspended");
+    } else {
+      console.log("Points:", demeritPoints);
+    }
   }
 }
 
-// Get the speed as input 
-const speed = 80;
+// Get the speed as input
+const carSpeed = parseFloat(prompt("Enter the car speed: "));
 
-// Calculate and display demerit points
-calculateDemeritPoints(speed);
+if (!isNaN(carSpeed) && carSpeed >= 0) {
+  calculateDemeritPoints(carSpeed);
+} else {
+  console.log("Invalid input. Please enter a valid number for car speed.");
+}
